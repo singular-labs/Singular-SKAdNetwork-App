7ed83a460e5b06e72bffbf88995531d0add983f1
Eyal Rabinovich <eyal@singular.net>
2020-06-25 18:52:22 +0300

    Improved naming and comments in the advertiser app

--- advertiser-sample-app/SingularAdvertiserSampleApp/ViewController.m
+++ advertiser-sample-app/SingularAdvertiserSampleApp/ViewController.m
@@ -10,7 +10,7 @@
 // Important: Add the AppTrackingTransparency.framework in the build phases tab.
 #import <AppTrackingTransparency/ATTrackingManager.h>
 
-// Don't forget to import this to have access to the SKAdnetwork
+// Don't forget to import this to have access to the SKAdnetwork framework
 #import <StoreKit/SKAdNetwork.h>
 
 @interface ViewController ()
@@ -25,30 +25,33 @@
 
 - (IBAction)showTrackingConsentDialog:(id)sender {
     // If the tracking authorization status is other than not determined, this means that the Tracking Consent dialog has already been shown.
-    // The `trackingAuthorizationStatus` persist the result of the Tracking Consent dialog and can only be changed through the settings app.
-    // Tracking Consent dialog is only shown once per install, meaning that call `requestTrackingAuthorizationWithCompletionHandler` won't show the dialog again.
+    // The `trackingAuthorizationStatus` persists the result of the Tracking Consent dialog and can only be changed through the iOS settings screen.
+    // Tracking Consent dialog is only shown once per install, meaning that calling `requestTrackingAuthorizationWithCompletionHandler` won't show the dialog again.
     if ([ATTrackingManager trackingAuthorizationStatus] != ATTrackingManagerAuthorizationStatusNotDetermined){
         [self alertTrackingConsentIsAlreadySet];
     }
     
-    // Checking the OS version before calling the Tracking Consent dialog, it's available only in v14.0 and above
+    // Checking the OS version before calling the Tracking Consent dialog, it's available only in iOS 14 and above
     if (@available(iOS 14, *)) {
         
         // Before showing the Tracking Consent dialog, you'll need to add the `Privacy - Tracking Usage Description` to your app's info.plist.
         // If you don't add it, an exception will be thrown.
         [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
-            // Here we get the result of the dialog,
+            // Here we get the result of the dialog
             // If `requestTrackingAuthorizationWithCompletionHandler` was called twice, the completion handler will be called twice, but will show the dialog on the first time.
             // On the second time this method is called the completion handler will be called with the value returned by `trackingAuthorizationStatus`
         }];
     } else {
-        // No need for to display the dialog in earlier versions
+        // No need to display the dialog in earlier versions
     }
 }
 
 - (IBAction)updateConversionValueClick:(id)sender {
-    // Once `registerAppForAdNetworkAttribution` is called for the first time (check the AppDelegate.m for explanations on `registerAppForAdNetworkAttribution`),
-    // A 24 hours window is opened the check for attribution data. Using `updateConversionValue` we can add a value (number between 0-63) to be sent with the attribution notification.
+    // Once `registerAppForAdNetworkAttribution` is called for the first time
+    // (check the AppDelegate.m for explanations on `registerAppForAdNetworkAttribution`),
+    // a 24 hours window is opened to update conversion value for attribution data.
+    
+    // Using `updateConversionValue` we can add a value (a number between 0-63) to be sent with the attribution notification.
     // Every time we call this method, we start a new 24 hours window until the notification is sent.
     // Please note that calling `updateConversionValue` is only effective in the first 24 hours since `registerAppForAdNetworkAttribution` is first called.
     // Any calls after 24 hours will not update the conversion value in the attribution notification.
@@ -65,7 +68,7 @@
 
 - (void)alertTrackingConsentIsAlreadySet {
     UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Can't Display Dialog"
-                                                                   message:@"Can't display Tracking Consent dialog because it was already been displayed. "
+                                                                   message:@"Can't display Tracking Consent dialog because it has already been displayed. "
                                                                            @"To see this dialog again please remove & reinstall this app."
                                                             preferredStyle:UIAlertControllerStyleAlert];
     
